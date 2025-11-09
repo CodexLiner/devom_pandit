369f709b90095a969208ba58d2acb68b083277ba
satyam astasnia <satyamatasnia@gmail.com>
2025-11-09 18:58:21 +0530

    Fix: Update Base URLs for Images and Documents
    - In `Constants.kt`, updated `IMAGE_BASE_URL` to point to the `/uploads/` directory.
    - Modified `DOCUMENT_BASE_URL` to point to the `/uploads/documents/` directory.

--- composeApp/src/commonMain/kotlin/com/devom/pandit/app/Constants.kt
+++ composeApp/src/commonMain/kotlin/com/devom/pandit/app/Constants.kt
@@ -15,8 +15,8 @@ const val REFRESH_TOKEN_KEY = "locallySavedRefreshTokenKey"
 const val UUID_KEY = "uuid"
 const val APPLICATION_ID = "applicationId"
 
-const val IMAGE_BASE_URL = "https://devom.blr1.digitaloceanspaces.com/"
-const val DOCUMENT_BASE_URL = "https://devom.blr1.digitaloceanspaces.com/documents/"
+const val IMAGE_BASE_URL = "https://devom.blr1.digitaloceanspaces.com/uploads/"
+const val DOCUMENT_BASE_URL = "https://devom.blr1.digitaloceanspaces.com/uploads/documents/"
 const val BASE_URL = "https://api.devom.co.in"
 const val ASSET_LINK_BASE_URL = "https://devom.co.in/pandit/"
 
