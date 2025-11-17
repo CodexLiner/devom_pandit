1de0d289ca58b8170259bd165db86515b9dc64a5
Gopal Meena <meenagopal24@gmail.com>
2025-11-18 00:47:20 +0530

    image alignment

--- composeApp/src/commonMain/kotlin/com/devom/pandit/app/ui/components/ImageViewer.kt
+++ composeApp/src/commonMain/kotlin/com/devom/pandit/app/ui/components/ImageViewer.kt
@@ -2,9 +2,11 @@ package com.devom.pandit.app.ui.components
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -19,6 +21,7 @@ import androidx.compose.runtime.MutableState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
@@ -38,37 +41,48 @@ import pandijtapp.composeapp.generated.resources.ic_close
 @Composable
 fun ImageViewer(viewImage: MutableState<Boolean>, selectedMedia: MutableState<Media?>) {
     if (viewImage.value) {
-        Dialog(onDismissRequest = { viewImage.value = false } , properties = DialogProperties(usePlatformDefaultWidth = false)) {
+        Dialog(
+            onDismissRequest = { viewImage.value = false },
+            properties = DialogProperties(usePlatformDefaultWidth = false)
+        ) {
             Box(
-                modifier = Modifier.padding(horizontal = 16.dp).size(400.dp).background(
-                    backgroundColor, shape = RoundedCornerShape(12.dp))
-
+                modifier = Modifier
+                    .padding(horizontal = 16.dp)
+                    .size(400.dp)
+                    .background(Color(0xFF121212), RoundedCornerShape(12.dp))
             ) {
-                Column(horizontalAlignment = Alignment.End) {
-                    Box(
-                        modifier = Modifier.wrapContentSize().background(primaryColor, CircleShape),
-                        contentAlignment = Alignment.TopEnd
-                    ) {
-                        IconButton(onClick = { viewImage.value = false }) {
-                            Image(
-                                colorFilter = ColorFilter.tint(whiteColor),
-                                painter = painterResource(Res.drawable.ic_close),
-                                contentDescription = "Close"
-                            )
-                        }
-                    }
+                IconButton(
+                    onClick = { viewImage.value = false },
+                    modifier = Modifier
+                        .align(Alignment.TopEnd)
+                        .padding(12.dp)
+                        .background(primaryColor, CircleShape)
+                ) {
+                    Image(
+                        painter = painterResource(Res.drawable.ic_close),
+                        contentDescription = "Close",
+                        colorFilter = ColorFilter.tint(whiteColor)
+                    )
+                }
 
-                    Spacer(modifier = Modifier.height(8.dp))
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center
+                ) {
                     if (selectedMedia.value?.documentType == SupportedFiles.VIDEO.type) {
                         selectedMedia.value?.documentUrl.toDevomDocument().StreamVideo()
-
-                    } else selectedMedia.value?.let { media ->
-                        AsyncImage(
-                            model = media.documentUrl.toDevomDocument(),
-                            modifier = Modifier.fillMaxWidth().wrapContentHeight()
-                                .clip(RoundedCornerShape(8.dp)),
-                            contentScale = ContentScale.FillWidth
-                        )
+                    } else {
+                        selectedMedia.value?.let { media ->
+                            AsyncImage(
+                                model = media.documentUrl.toDevomDocument(),
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .wrapContentHeight()
+                                    .clip(RoundedCornerShape(8.dp)),
+                                contentScale = ContentScale.FillWidth
+                            )
+                        }
                     }
                 }
             }
